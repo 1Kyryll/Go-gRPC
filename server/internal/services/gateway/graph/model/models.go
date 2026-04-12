2bc11bf72c873d6f8396389f63e11f9b4d7d7964
Kyryll <kyryllupwork@gmail.com>
2026-04-12 13:25:35 +0200

    feat: add root resolver and model mapping for GraphQL types

--- server/internal/services/gateway/graph/model/models.go
+++ server/internal/services/gateway/graph/model/models.go
@@ -1,1 +1,72 @@
 package model
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/1kyryll/go-grpc/internal/services/common/sqlc"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+// MapMenuItem converts sqlc.MenuItem to the appropriate GraphQL MenuItem type
+func MapMenuItem(item sqlc.MenuItem) MenuItem {
+	id := fmt.Sprintf("%d", item.ID)
+
+	var desc *string
+	if item.Description.Valid {
+		desc = &item.Description.String
+	}
+
+	price, _ := item.Price.Float64Value()
+	priceFloat := price.Float64
+
+	isAvailable := item.IsAvailable.Valid && item.IsAvailable.Bool
+
+	if item.Category == "DRINK" {
+		isAlcoholic := item.IsAlcoholic.Valid && item.IsAlcoholic.Bool
+		return DrinkItem{
+			ID:          id,
+			Name:        item.Name,
+			Description: desc,
+			Price:       priceFloat,
+			Category:    MenuCategoryDrink,
+			IsAvailable: isAvailable,
+			IsAlcoholic: isAlcoholic,
+		}
+	}
+
+	var allergens []string
+	if item.ContainsAllergens != nil {
+		allergens = item.ContainsAllergens
+	}
+
+	var category MenuCategory
+	switch item.Category {
+	case "APPETIZER":
+		category = MenuCategoryAppetizer
+	case "MAIN":
+		category = MenuCategoryMain
+	case "DESSERT":
+		category = MenuCategoryDessert
+	default:
+		category = MenuCategory(item.Category)
+	}
+
+	return FoodItem{
+		ID:                id,
+		Name:              item.Name,
+		Description:       desc,
+		Price:             priceFloat,
+		Category:          category,
+		IsAvailable:       isAvailable,
+		ContainsAllergens: allergens,
+	}
+}
+
+// TimestampToTime converts pgtype.Timestamptz to *time.Time
+func TimestampToTime(ts pgtype.Timestamptz) *time.Time {
+	if !ts.Valid {
+		return nil
+	}
+	return &ts.Time
+}
--- server/internal/services/gateway/graph/resolver.go
+++ server/internal/services/gateway/graph/resolver.go
@@ -1,8 +1,27 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require
-// here.
+import (
+	"sync"
 
-type Resolver struct{}
+	"github.com/1kyryll/go-grpc/internal/services/common/gen/orders"
+	"github.com/1kyryll/go-grpc/internal/services/common/sqlc"
+)
+
+type Resolver struct {
+	queries    *sqlc.Queries
+	grpcClient orders.OrderServiceClient
+
+	// For subscriptions
+	mu                sync.Mutex
+	orderSubscribers  map[chan *orders.Order]struct{}
+	statusSubscribers map[chan *orders.Order]struct{}
+}
+
+func NewResolver(queries *sqlc.Queries, grpcClient orders.OrderServiceClient) *Resolver {
+	return &Resolver{
+		queries:           queries,
+		grpcClient:        grpcClient,
+		orderSubscribers:  make(map[chan *orders.Order]struct{}),
+		statusSubscribers: make(map[chan *orders.Order]struct{}),
+	}
+}
