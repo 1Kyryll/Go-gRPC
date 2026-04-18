f0727d4a9961a1cc235438071c825bf114b522bf
Kyryll <kyryllupwork@gmail.com>
2026-04-18 14:04:20 +0200

    feat: start Outbox Relay in Orders service

--- server/cmd/orders/main.go
+++ server/cmd/orders/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
@@ -12,6 +15,7 @@ import (
 	"github.com/1kyryll/go-grpc/internal/common/sqlc"
 	handler "github.com/1kyryll/go-grpc/internal/services/orders/handlers"
 	service "github.com/1kyryll/go-grpc/internal/services/orders/services"
+	outbox "github.com/1kyryll/go-grpc/internal/services/outbox"
 	"google.golang.org/grpc"
 )
 
@@ -27,7 +31,7 @@ func main() {
 	queries := sqlc.New(pool)
 	ordersService := service.NewOrdersService(pool, queries)
 
-	// Start gRPC server for Kitchen
+	// Start gRPC server for Gateway subscriptions
 	go func() {
 		lis, err := net.Listen("tcp", ":9000")
 		if err != nil {
@@ -41,7 +45,28 @@ func main() {
 		}
 	}()
 
+	// Start outbox relay (publishes to Kafka)
+	kafkaBrokers := os.Getenv("KAFKA_BROKERS")
+	if kafkaBrokers == "" {
+		kafkaBrokers = "loclahost:9092"
+	}
+	relay := outbox.NewOutboxRelay(queries, strings.Split(kafkaBrokers, ","))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go relay.Run(ctx)
+	log.Println("Outbox relay started")
+
 	// Start HTTP server for frontend
-	httpServer := NewHTTPServer(":8080", ordersService)
-	httpServer.Run()
+	go func() {
+		httpServer := NewHTTPServer(":8080", ordersService)
+		httpServer.Run()
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("Orders service is running. Press Ctrl+C to exit.")
+	<-sig
+	cancel()
 }
